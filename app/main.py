2ba7a40d2482771404a973aff208f0f3168f71fe
Marian Hrytsan <gricanmaran@gmail.com>
2026-02-06 02:43:20 +0200

    Corect version 1.1

--- app/main.py
+++ app/main.py
@@ -13,10 +13,10 @@ def shop_trip():
     customer_list = []
     shop_list = []
     # create car object
-    for i, customer_car in enumerate(config_data["customers"]):
+    for customer_car in config_data["customers"]:
         car_list.append(Car(customer_car["car"]["brand"],
                             customer_car["car"]["fuel_consumption"]))
-    for customer_info in config_data["customers"]:
+    for i, customer_info in enumerate(config_data["customers"]):
         customer_list.append(Customer(customer_info["name"],
                                       customer_info["product_cart"],
                                       customer_info["location"],
@@ -29,7 +29,7 @@ def shop_trip():
 
     for customer in customer_list:
         print(f"{customer.name} has {customer.money} dollars")
-        cheap_shop = [1000, 0, 0]
+        cheap_shop = [float("inf"), 0, 0]
         for i, shop in enumerate(shop_list):
             distance = math.fabs(math.sqrt(math.pow(customer.location[0] - shop.location[0],2) +
                                            math.pow(customer.location[1] - shop.location[1],2)))
@@ -46,18 +46,21 @@ def shop_trip():
         if customer.money < cheap_shop[0]:
             print(f"{customer.name} doesn't have enough money to make a purchase in any shop")
         else:
-            print(f"{customer.name} rides to shop {shop_list[cheap_shop[1]].name} \n")
+            print(f"{customer.name} rides to {shop_list[cheap_shop[1]].name} \n")
+            home_location =customer.location
+            customer.location = shop_list[cheap_shop[1]].location
             print(f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
             print(f"Thanks, {customer.name}, for your purchase!")
             print(f"You have bought:")
             for buy_product in customer.product_cart:
-                print(f"{customer.product_cart[buy_product]} {buy_product} for "
+                print(f"{customer.product_cart[buy_product]} {buy_product}s for "
                       f"{customer.product_cart[buy_product] * shop_list[cheap_shop[1]].products[buy_product]} dollars")
             print(f"Total cost is {cheap_shop[2]} dollars")
             print(f"See you again!\n")
             print(f"{customer.name} rides home")
+            customer.location = home_location
             customer.money = customer.money - cheap_shop[0]
-            print(f"{customer.name} now has {customer.money} dollars\n")
+            print(f"{customer.name} now has {customer.money:.2f} dollars\n")
 
 
 
